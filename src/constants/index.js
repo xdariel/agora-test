7b60cc534741e6aefd6a0d3eaf9fb84bcdede556
dariel87@gmail.com <Neontiger1*>
2021-04-13 09:12:13 -0400

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,14 @@
+export const rtc = {
+  // For the local client
+  client: null,
+  // For the local audio and video tracks
+  localAudioTrack: null,
+  localVideoTrack: null,
+};
+
+export const options = {
+  // Pass your app ID here
+  appId: "6f165aeeeefe4bccba63fd88dc5f12e3",
+  // Pass a token if your project enables the App Certificate
+  token: "0066f165aeeeefe4bccba63fd88dc5f12e3IABVQuUxvctCaPg53I1b/YhM43IeY2h4KoOUQGSe9hVk86axnYYAAAAAEADBVZQ5C+B2YAEAAQAL4HZg",
+};
